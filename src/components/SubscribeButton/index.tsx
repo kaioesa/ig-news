f92a9d07b32acdc5c288ac65685e1f8c4ba4ef9f
xkaio@hotmail.com <xkaio@hotmail.com>
2022-06-19 03:57:37 -0300

    Initial commit

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.scss";
+
+export default function SubscribeButton() {
+  return (
+      <button className={styles.subscribeButton}>
+        Subscribe now
+      </button>
+  );
+}
